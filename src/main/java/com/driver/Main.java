94f20861b4f98475b21afd17331853e08bf1d883
Md <mdmustaqeem820@gmail.com>
2026-01-14 23:48:10 +0530

    Assignment Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static class Profuct{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Profuct p = new Profuct();
+        System.out.println(p.product(2,2));
+        System.out.println(p.product(2,2,2));
+        System.out.println(p.product(2.2,2.2));
+    }
 
 }
